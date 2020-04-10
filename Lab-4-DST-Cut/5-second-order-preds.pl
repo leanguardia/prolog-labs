1ffc47793f66b2729f1e11c5760ef709b5ed3e02
Leandro Guardia <lean.guardia03@gmail.com>
2020-04-10 14:44:54 +0100

    Second order logi

--- Lab-4-DST-Cut/5-second-order-preds.pl
+++ Lab-4-DST-Cut/5-second-order-preds.pl
@@ -1,1 +1,19 @@
 % Book 3.7
+
+% p. 68 Write program which sorts and removes 
+%       duplicates from a list, using set of.
+
+% Epix Fail
+% sort_and_unique([],[]).
+% sort_and_unique([H|T],Result):-
+%     sort_and_unique(T,Result)
+%     setof(_,_,[T|Result]).
+
+% Solution: 
+sort(List,SortedList):-
+  setof(X,element(X,List),SortedList).
+  
+  element(X,[X|Ys]).
+  
+  element(X,[Y|Ys]):-
+  element(X,Ys). 
