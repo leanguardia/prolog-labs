89d76a74cdc894168a97fd02f090977c7f940de5
Leandro Guardia <lean.guardia03@gmail.com>
2020-04-11 13:19:12 +0100

    TODO for River Cross Tastk

--- Example-River-Cross/solution.pl
+++ Example-River-Cross/solution.pl
@@ -9,6 +9,11 @@ state([man:M, fox:F, hen:H, rye:R]) :-
     member(H,[0,1]),
     member(R,[0,1]).
 
+% TODO
+% move(X,Y)
+% moves(X,Y,W):-!
+% moves(X,Y,W):-
+
 /** <examples>
 ?- state([man:0,fox:0,hen:0,rye:0]).
 */
