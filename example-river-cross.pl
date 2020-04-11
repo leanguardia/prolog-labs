411dc3b4ad72b5ced4c292eeff8537cda5f42428
Leandro Guardia <lean.guardia03@gmail.com>
2020-04-11 12:23:47 +0100

    Last failed update

--- example-river-cross.pl
+++ example-river-cross.pl
@@ -6,6 +6,7 @@ element(none).
 eats(fox,hen).
 eats(hen,rye).
 
+is_safe([]).
 is_safe([_]).
 is_safe([A,B]) :- \+ eats(A,B), \+eats(B,A) .
 is_safe([_,_,_]).
@@ -20,21 +21,26 @@ action(LeftBank, _, Direction) :-
 % cross([LeftH|LeftT]):-safe(With).
 % return()
 
+rotate([H|T],R):-append(T,[H],R).
+
 find_travels(_LeftBank, RightBank, _T) :- length(RightBank,4).
 find_travels(LeftBank, RightBank, T) :- % Add Negative case in each travel
 	action(LeftBank, RightBank, Direction),
 	( Direction = way    -> delete(LeftBank,man,[LH|LT]),
-        					is_safe(LT),
-        					find_travels(LT,[man,LH|RightBank],T+1)
+        (   is_safe(LT) 	-> append(T,[LH],TPlus),find_travels(LT,[man,LH|RightBank],TPlus)
+        ;   otherwise		-> rotate(LeftBank,LR),find_travels(LR,RightBank,T)
+        )
     ; Direction = return -> delete(RightBank,man,[RH|RT]),
-        					is_safe(RT),
-        					find_travels([man,RH|LeftBank],RT,T+1)
+        (   is_safe(RT)     -> append(T,[RH],TPlus),find_travels([man,RH|LeftBank],RT,TPlus)
+        ;   otherwise		-> rotate(RightBank,RR),find_travels(LeftBank,RR,T)
+        )
 	).
 
 
 /** <examples>
 ?- is_safe([rye,fox]).
-?- find_travels([man,fox],[],0).
-?- action([man,fox,hen,rye],[],Travels).
+?- trace, (find_travels([man,fox,hen,rye],[],T)).
+?- find_travels([man,fox,hen,rye],[],T).
+?- action([fox,hen,rye,man],[],Travels).
 ?- findall(E, element(E), List).
 */
