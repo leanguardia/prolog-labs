7d27f2aab8655c5ac37361f31cfe29fd6efd1153
Leandro Guardia <lean.guardia03@gmail.com>
2020-04-11 12:40:51 +0100

    River Cross solution

---
+++ Example-River-Cross/solution.pl
@@ -0,0 +1,14 @@
+eats(fox,hen).
+eats(hen,rye).
+
+% This is a way to represent states.
+% Binding man, fox ... with either 0 or 1.
+state([man:M, fox:F, hen:H, rye:R]) :-
+    member(M,[0,1]),
+    member(F,[0,1]),
+    member(H,[0,1]),
+    member(R,[0,1]).
+
+/** <examples>
+?- state([man:0,fox:0,hen:0,rye:0]).
+*/
